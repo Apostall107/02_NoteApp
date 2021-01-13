94eecaaf103627df816f95a68c660ee6b09d3e3e
Apostall107 <Apostalls107@gmail.com>
2021-01-13 18:42:52 +0200

    complete

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Button_Clear = new System.Windows.Forms.Button();
+            this.Button_Add = new System.Windows.Forms.Button();
+            this.Button_Read = new System.Windows.Forms.Button();
+            this.Button_Delete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Message";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(15, 25);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(325, 20);
+            this.txtTitle.TabIndex = 2;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(15, 64);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(325, 249);
+            this.txtMessage.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(346, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(222, 417);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Button_Clear
+            // 
+            this.Button_Clear.Location = new System.Drawing.Point(15, 319);
+            this.Button_Clear.Name = "Button_Clear";
+            this.Button_Clear.Size = new System.Drawing.Size(137, 44);
+            this.Button_Clear.TabIndex = 5;
+            this.Button_Clear.Text = "Clear";
+            this.Button_Clear.UseVisualStyleBackColor = true;
+            this.Button_Clear.Click += new System.EventHandler(this.Button_Clear_Click);
+            // 
+            // Button_Add
+            // 
+            this.Button_Add.Location = new System.Drawing.Point(203, 319);
+            this.Button_Add.Name = "Button_Add";
+            this.Button_Add.Size = new System.Drawing.Size(137, 44);
+            this.Button_Add.TabIndex = 6;
+            this.Button_Add.Text = "Add";
+            this.Button_Add.UseVisualStyleBackColor = true;
+            this.Button_Add.Click += new System.EventHandler(this.Button_Add_Click);
+            // 
+            // Button_Read
+            // 
+            this.Button_Read.Location = new System.Drawing.Point(15, 381);
+            this.Button_Read.Name = "Button_Read";
+            this.Button_Read.Size = new System.Drawing.Size(137, 44);
+            this.Button_Read.TabIndex = 7;
+            this.Button_Read.Text = "Read";
+            this.Button_Read.UseVisualStyleBackColor = true;
+            this.Button_Read.Click += new System.EventHandler(this.Button_Read_Click);
+            // 
+            // Button_Delete
+            // 
+            this.Button_Delete.Location = new System.Drawing.Point(203, 381);
+            this.Button_Delete.Name = "Button_Delete";
+            this.Button_Delete.Size = new System.Drawing.Size(137, 43);
+            this.Button_Delete.TabIndex = 8;
+            this.Button_Delete.Text = "Delete";
+            this.Button_Delete.UseVisualStyleBackColor = true;
+            this.Button_Delete.Click += new System.EventHandler(this.Button_Delete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(577, 450);
+            this.Controls.Add(this.Button_Delete);
+            this.Controls.Add(this.Button_Read);
+            this.Controls.Add(this.Button_Add);
+            this.Controls.Add(this.Button_Clear);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Button_Clear;
+        private System.Windows.Forms.Button Button_Add;
+        private System.Windows.Forms.Button Button_Read;
+        private System.Windows.Forms.Button Button_Delete;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace _02_NoteApp
 {
     public partial class Form1 : Form
     {
+        DataTable table;
+
         public Form1()
         {
             InitializeComponent();
+
+            table = new DataTable();
+            table.Columns.Add("Title", typeof(String));
+            table.Columns.Add("Messages", typeof(String));
+            dataGridView1.DataSource = table;
+
+            dataGridView1.Columns["Messages"].Visible = false;//hide message column
+            dataGridView1.Columns["Title"].Width = 218;
+
+        }
+
+        private void Button_Clear_Click(object sender, EventArgs e)
+        {
+            txtTitle.Clear();
+            txtMessage.Clear();
+        }
+
+        private void Button_Add_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(txtTitle.Text, txtMessage.Text);
+
+            txtTitle.Clear();
+            txtMessage.Clear();
+        }
+
+        private void Button_Read_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;//geting current chosen cell row index
+
+            if (index > -1)//1st index is 0 so checking if any cell chosen
+            {
+                txtTitle.Text = table.Rows[index].ItemArray[0].ToString();//item array[0] is 1st column 
+                txtMessage.Text = table.Rows[index].ItemArray[1].ToString();//2nd
+            }
+
+
+        }
+
+        private void Button_Delete_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;//geting current chosen cell row index
+
+            table.Rows[index].Delete();
+
+
         }
     }
 }
